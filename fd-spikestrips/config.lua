bfddbcc8eed33006418ed48d8b5c9ea97ca43b93
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-12-25 07:24:08 -0500

    updated

--- fd-spikestrips/config.lua
+++ fd-spikestrips/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.UseTarget = false -- true = uses qb-target to remove spikes, false = removes automatically after a set time.
-Config.AllowSpikesOnlyForPolice = false -- If spikes are only usable by police.
+Config.UseTarget = true -- true = uses qb-target to remove spikes, false = removes automatically after a set time.
+Config.AllowSpikesOnlyForPolice = true -- If spikes are only usable by police.
 Config.MaxSpikes = 5 -- How many spikes a player can place at the same time
-Config.InstantlyPop = 1 -- 1 == It will instantly pop the tires, false = It will not instantly pop the tires.
-Config.DeleteTime = 30 -- How many seconds for the spikestrip to delete after it has been placed (In seconds). 
+Config.InstantlyPop = false -- 1 == It will instantly pop the tires, false = It will not instantly pop the tires.
+Config.DeleteTime = 120 -- How many seconds for the spikestrip to delete after it has been placed (In seconds). 
